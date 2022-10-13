eef0161598bd7eaa1c252bcc043a9313be52a30a
鹿琛 <1143729736@qq.com>
2022-10-13 19:57:41 +0800

    数据列表展示

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+axios.defaults.baseURL = 'http://114.116.10.5:8080'
+Vue.prototype.$http = axios
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an about page！！！！，修改以下，自动部署并更新</h1>
+  <div>
+    <el-table :data="peopleList" border style="width: 100%">
+      <el-table-column prop="name" label="Name" width="180" align="center"></el-table-column>
+      <el-table-column prop="state" label="State" width="180" align="center"></el-table-column>
+      <el-table-column prop="salary" label="Salary" align="center"></el-table-column>
+      <el-table-column prop="grade" label="Grade" align="center"></el-table-column>
+      <el-table-column prop="room" label="Room" align="center"></el-table-column>
+      <el-table-column prop="telnum" label="Telnum" align="center"></el-table-column>
+      <el-table-column prop="picture" label="Picture" align="center"></el-table-column>
+      <el-table-column prop="keywords" label="Keywords" align="center"></el-table-column>
+    </el-table>
   </div>
 </template>
+
+<script>
+export default {
+  async created () {
+    // 人员列表数据
+    const { data } = await this.$http.get('/people/fetchList')
+    this.peopleList = data
+  },
+  data () {
+    return {
+      peopleList: []
+    }
+  }
+}
+</script>
+
+<style>
+</style>
